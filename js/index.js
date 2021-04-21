e9d75a098e61d79d3cdf590932c86cca2f6dcf34
bulichova <bulichova84@gmail.com>
2021-04-21 23:53:28 +0300

    game2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,56 @@
 import refs from './refs.js'
 console.log(refs.choiceBtns)
+const CHOICES = [
+  { name: 'paper', bits: 'rock' },
+  { name: 'scissors', bits: 'paper' },
+  { name: 'rock', bits: 'scissors' },
+]
 
 // Перебираем кнопки и вешаем на них Слушатель событий
 
 refs.choiceBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
     const choiceName = btn.dataset.choice
-    console.log("я нажал на",choiceName)
+    console.log('я нажал на', choiceName)
+    const userChoice = CHOICES.find((elem) => {
+      return elem.name === choiceName
+    })
+    toChoose(userChoice)
   })
 })
+
+// функия, которая делает выбор
+
+function toChoose(value) {
+  const compChoose = randomChoose()
+  console.log(`выбор компьютера`, compChoose)
+  console.log(` выбор пользователя`, value)
+}
+
+// функия, которая показывает результат
+function showResults() {
+  console.log(refs.resultsDivs)
+  refs.resultsDivs.forEach((div, index) => {
+    console.log(div)
+    console.log(index)
+    const item = document.createElement('div')
+    console.log(item)
+    item.classList.add('choice')
+    const image = document.createElement('img')
+    console.log(image)
+    item.appendChild(image)
+  })
+}
+showResults()
+
+// функция рандомного выбора компьютера
+
+function randomChoose() {
+  let random = Math.random() * CHOICES.length
+  console.log(random)
+  let floorRandom = Math.floor(random)
+  console.log(floorRandom)
+  console.log(CHOICES[floorRandom])
+  return CHOICES[floorRandom]
+}
+toChoose()
--- js/refs.js
+++ js/refs.js
@@ -1,5 +1,14 @@
 export default {
   // btnRules: document.querySelector(""),
   choiceBtns: document.querySelectorAll('.choice-btn'),
-  sectionGame: document.querySelectorAll(".game")
+  sectionGame: document.querySelectorAll('.game'),
+  resultsDiv: document.querySelector('.results'),
+  resultsDivs: document.querySelectorAll('.results__result'),
+  resultsWinner: document.querySelector('.results__winner'),
+  resultsText: document.querySelector('.results__text'),
+  playAgain: document.querySelector('.play-again'),
+  rulesBtn: document.querySelector('.rules-btn'),
+  scoreNumber: document.querySelector('.score-number'),
+  modal: document.querySelector('.modal'),
+  closeBtn: document.querySelector('.close-btn'),
 }
